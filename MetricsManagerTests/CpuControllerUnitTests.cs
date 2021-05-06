ed9be285f35addf5a9c4d2dd73ba04eb7d923e4d
Semenow94 <semenow94@gmail.com>
2021-05-06 07:27:15 +0300

    Manager Tests

--- MetricsManagerTests/CpuControllerUnitTests.cs
+++ MetricsManagerTests/CpuControllerUnitTests.cs
@@ -28,5 +28,19 @@ namespace MetricsManagerTests
             // Assert
             _ = Assert.IsAssignableFrom<IActionResult>(result);
         }
+
+        [Fact]
+        public void GetMetricsFromCluster_ReturnsOk()
+        {
+            //Arrange
+            var fromTime = TimeSpan.FromSeconds(0);
+            var toTime = TimeSpan.FromSeconds(100);
+
+            //Act
+            var result = controller.GetMetricsFromAllCluster(fromTime, toTime);
+
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
     }
 }
---
+++ MetricsManagerTests/NetworkControllerUnitTests.cs
@@ -0,0 +1,48 @@
+﻿using MetricsManager.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MetricsManagerTests
+{
+    public class NetworkControllerUnitTests
+    {
+        private NetworkMetricsController controller;
+
+        public NetworkControllerUnitTests()
+        {
+            controller = new NetworkMetricsController();
+        }
+
+        [Fact]
+        public void GetMetricsFromAgent_ReturnsOk()
+        {
+            //Arrange
+            var agentId = 1;
+            var fromTime = TimeSpan.FromSeconds(0);
+            var toTime = TimeSpan.FromSeconds(100);
+
+            //Act
+            var result = controller.GetMetricsFromAgent(agentId, fromTime, toTime);
+
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+
+        [Fact]
+        public void GetMetricsFromCluster_ReturnsOk()
+        {
+            //Arrange
+            var fromTime = TimeSpan.FromSeconds(0);
+            var toTime = TimeSpan.FromSeconds(100);
+
+            //Act
+            var result = controller.GetMetricsFromAllCluster(fromTime, toTime);
+
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+    }
+}
